30d961c0d00dd93017f7afbf1cca3abdfa90f9b7
Steve Jones <steve@squaregoldfish.co.uk>
2021-05-09 13:47:38 +0200

    New Pluto notebook - summary

--- diel_stats/explore_cell_time_series.jl
+++ diel_stats/explore_cell_time_series.jl
@@ -1,109 +1,61 @@
 ### A Pluto.jl notebook ###
-# v0.14.0
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
 
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    quote
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
-        el
-    end
-end
+# ╔═╡ 9c288f31-bf08-4817-b467-19cde8a9d2b6
+using PlutoUI
 
-# ╔═╡ 84e8ecfc-f90d-45e9-b14a-f74d09a5e3ad
-begin
-	using PlutoUI
-	using SQLite
-  	using DataFrames
-  	using Plots
-  	plotly()
-
-  	db = SQLite.DB("cell_time_series.sqlite")
-end
-
-# ╔═╡ 05f60082-9554-11eb-18b0-c1e9d5ba8d3b
+# ╔═╡ e7955031-cf60-44a9-8f5f-1162862ecbc8
 md"""
 # SOCAT Cell Time Series
 This notebook explores various aspects of the time series for grid cells in the SOCAT dataset.
 
-Before running this notebook, you must run `make_time_series_cells.jl`. This creates an SQLite database containing extracted time series for grid cells. Each time series represents at most one voyage (identified by its Expo Code) and covers one day. Where a voyage passes through multiple cells and/or multiple days, separate records for each are created.
+Before running this notebook, you must run the following three scripts:
+- `make_socat_database.jl` (needs a copy of the main SOCAT tsv file in the current directory)
+- `make_timeseries_count.jl`
+- `mean_sd_minute.jl`
+
+These scripts create the database and netCDF files used by this notebook.
 
 ## Introduction
 
 ### Database Structure
-The database contains one record for each extracted time series. Each record contains the following fields:
+`make_socat_database.jl` creates `socat.sqlite` which contains a simplified version of the SOCAT dataset. It contains one table (`socat`) with a record for each SOCAT observation. The table contains the following fields:
 
-| Field    | Description                                                          |
-|:---------|:---------------------------------------------------------------------|
-| lon      | The cell longitude, from 1 to 360 (0.5° to 359.5°)                   |
-| lat      | The cell latitude, from 1 to 180 (-89.5° to 89.5°)                   |
-| expocode | The Expo Code of the voyage from which the time series was extracted |
-| year     | The year of the time series                                          |
-| doy      | The day of year of the time series                                   |
-| series   | The time series data                                                 |
+| Field       | Description                                                          |
+|:------------|:---------------------------------------------------------------------|
+| lonindex    | The longitude cell index†                                            |
+| latindex    | The latitude cell index†                                             |
+| year        | The year in which the measurement was taken                          |
+| dayofyear   | The day of the year on which the measurement was taken‡              |
+| minuteofday | The minute of the day on which the measurement was taken             |
+| expocode    | The Expo Code of the voyage during which the measurement was taken   |
+| sst         | The measured sea surface temperature                                 |
+| sss         | The measured sea surface salinity                                    |
+| fco2        | The measured fCO₂                                                    |
 
-The day of year (`doy`) ignores leap years - dates on or after 29th February in leap years are decremented by 1 to align them with other years. This may lead to multiple time series for 28th February in a given year, but this shouldn't be a problem.
+†The index represents a 1° cell, ranging from `1 = 0.5°` through `360 = 359.5°` for longitude and `1 = -89.5°` to `180 = 89.5°` for latitude.
 
-The time series is a JSON string of the form:
-```
-[
-  {
-    "second":72203,
-    "fco2":357.842
-  },
-  {
-    "second":72213,
-    "fco2":357.969
-  }
-]
-```
-where `seconds` is the time of day in seconds (1 - 86,400), and `fco2` is the fCO_2_ measurement at that time.
+‡The `dayofyear` ignores leap years - dates on or after 29th February in leap years are decremented by 1 to align them with other years. This may lead to multiple time series for 28th February in a given year, but this shouldn't be a problem.
 
-## Exploring the time series
-There are a lot of things for us to look at. First, let's set up all the packages we'll need and get a connection to the database.
+## Data Summary
+`make_time_series_count.jl` and `mean_sd_minute.jl` compute statistics of the cells' time series. You can look at these in detail below, but for now here is a summary.
+
+### Number of time series
+The map below shows the number of days containing observations in each 1° grid cell, taken from the SOCATv2020 dataset.
+$(LocalResource("./time_series_count.png"))
+
+### Mean minute
+This map shows the mean minute of the day in which observations wjere taken in each grid cell across the entire SOCAT dataset. The lack of a clear pattern indicates that there is no systematic bias across the dataset regarding observation time, which is expected.
+$(LocalResource("./mean_minute.png"))
+
+### R̅
+The R̅ computed for a grid cell is a measure of how widely distributed the measurements are throughout the day. R̅ is a value between 0 and 1, where 0 indicates a wide distribution throughout the day and 1 indicating that measurements are concentrated around a particular time. The map below shows the R̅ for each grid cell.
+$(LocalResource("./Rbar.png"))
 """
-
-# ╔═╡ 6653866e-2af2-490a-bb59-5f0097ad8fc3
-md"""
-### Explore time series
-Choose a latitude and longitude to see what time series the corresponding grid cell contains.
-
-Longitude (0:360):
-$(@bind lon NumberField(0:360))
-
-Latitude (-90:90):
-$(@bind lat NumberField(-90.0:90.0))
-
-"""
-
-# ╔═╡ cf328521-8217-4b47-885a-6592f4560591
-begin
-	lonindex = lon +  1
-	if lonindex > 359
-		lonindex = 359
-	end
-	
-	latindex = lat + 91
-	if latindex > 180
-		latindex = 180
-	end
-
-	cellseries = DataFrame(
-		DBInterface.execute(db,
-			"SELECT expocode, year, doy, series FROM timeseries " *
-			"WHERE lon = $(lonindex) AND lat = $(latindex)")
-		)
-end
-
-# ╔═╡ f59113ea-ded2-4af2-a72e-9c561db0e729
-matchedseries = nrow(cellseries)
 
 # ╔═╡ Cell order:
-# ╟─05f60082-9554-11eb-18b0-c1e9d5ba8d3b
-# ╠═84e8ecfc-f90d-45e9-b14a-f74d09a5e3ad
-# ╟─6653866e-2af2-490a-bb59-5f0097ad8fc3
-# ╠═cf328521-8217-4b47-885a-6592f4560591
-# ╠═f59113ea-ded2-4af2-a72e-9c561db0e729
+# ╠═9c288f31-bf08-4817-b467-19cde8a9d2b6
+# ╟─e7955031-cf60-44a9-8f5f-1162862ecbc8
